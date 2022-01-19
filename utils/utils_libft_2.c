5058586755c5cd5f2dab3c72fca9a8fe4b675cb5
coviccinelle <thi-phng@student.42.fr>
2022-01-19 12:00:52 +0100

    ft_strdup added

--- utils/utils_libft_2.c
+++ utils/utils_libft_2.c
@@ -29,3 +29,27 @@ void	ft_puterror_fd(char *error, char *s, char *error2)
 	ft_putstr_fd(error2, 2);
 	ft_putchar_fd('\n', 2);
 }
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (s1[size])
+	{
+		size++;
+	}
+	str = malloc((size + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
