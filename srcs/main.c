ee9d16fee5e24c2191e85cffa1d4e8183e5cdf54
coviccinelle <thi-phng@student.42.fr>
2022-01-28 13:33:53 +0100

    signal done, ctrl ^D done

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:33:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/01/25 15:50:32 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/01/28 13:29:03 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,24 +98,27 @@ char	*ft_readline_input(char *line)
 	signal(SIGQUIT, ft_sigquit_ctr_d);
 	line = readline("\033[1;33m~Minishell$\033[0m ");
 	if (!line)
-		//ft_exit(NULL, env);
-		printf("Ft_exit please\n");
+	{
+		printf("faut faire la fct exit ctrl^D\n");
+		exit(0);
+	}
 	return (line);
 }
 
 
-void	ft_init_mini(t_mini mini)
+void	ft_init_mini(t_mini *mini)
 {
-	mini.env = NULL;
-	mini.line = NULL;
-	mini.ret = 0;
-	mini.builtin = 0;
-	mini.n_cmd = 0;
-	mini.fork = 0;
-	mini.pipes = 0;
-	mini.heredoc = 0;
-	mini.stop = 0;
-	mini.next = NULL;
+	printf("here\n");
+	mini->env = NULL;
+	mini->line = NULL;
+	mini->ret = 0;
+	mini->builtin = 0;
+	mini->n_cmd = 0;
+	mini->fork = 0;
+	mini->pipes = 0;
+	mini->heredoc = 0;
+	mini->stop = 0;
+	mini->next = NULL;
 }
 
 
@@ -156,23 +159,25 @@ void	minishell(t_mini *mini)
 
 int	main(int ac, char **av, char **envp)
 {
-	t_mini		*mini;
+	t_mini		mini;
 	(void)av;
 //	g_n_exit = 0;
 
-	mini = NULL;
-	ft_init_mini(*mini);
-	mini->env = ft_env_cpy(envp);
+	//mini = NULL;
+	printf("000\n");
+	ft_init_mini(&mini);
+	printf("110\n");
+	mini.env = ft_env_cpy(envp);
 	if (ac != 1)
 		return (printf("Error: Invalid argument\nHint: only ./minishell\n"), 1);
 	init_shell();
-	while (mini->stop == 0)
+	while (mini.stop == 0)
 	{
-		mini->line = ft_readline_input(mini->line);
-		add_history(mini->line);
-		parsing(mini);
-		if (mini->line)
-			minishell(mini);
+		mini.line = ft_readline_input(mini.line);
+		add_history(mini.line);
+		parsing(&mini);
+		if (mini.line)
+			minishell(&mini);
 		//free_token(mini.line);
 	}
 	//free_env(mini.env);
