5093f7ca7235323366c77e31a5b9e1fe33a53079
thi-phng <thi-phng@student.42.fr>
2022-02-09 12:21:07 +0100

    unset not segfaut plus mais not working on PATH

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:34:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/02/08 20:39:03 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/02/09 12:19:23 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@
 #include <errno.h>  
 #include<sys/wait.h>
 
-
 # define SEPARATORS " '\"|><"
+
 
 // *** // STRUCTURE  // *** //
 
