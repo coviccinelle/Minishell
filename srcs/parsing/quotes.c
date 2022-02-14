e2fa06189efac04b13fac8e191e5624b60d08847
thi-phng <thi-phng@student.42.fr>
2022-02-14 12:54:07 +0100

    done quote tadaaaa 2

--- srcs/parsing/quotes.c
+++ srcs/parsing/quotes.c
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 16:38:51 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/02/14 12:48:21 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/02/14 12:53:37 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "../../minishell.h"
 
 
-int	ft_check_2rd_quote(char *argv, int a)
+int	ft_check_2rd_quote(char *str, int a)
 {
 	char	c;
 	int		i;
@@ -24,14 +24,14 @@ int	ft_check_2rd_quote(char *argv, int a)
 	i = 0;
 	c = a;
 	p = 0;
-	while (argv[i])
+	while (str[i])
 	{
-		if (i != 0 && argv[i + 1] && argv[i + 1] == c)
+		if (i != 0 && str[i + 1] && str[i + 1] == c)
 		{
 			p++;
 			break ;
 		}
-		if (argv[i] == c)
+		if (str[i] == c)
 			p++;
 		i++;
 	}
@@ -69,22 +69,22 @@ int	ft_check_2rd_quote_2(char *line, int c)
 }
 
 
-void	ft_pass_squote(char *argv, int *i)
+void	ft_pass_squote(char *str, int *i)
 {
-	if (argv[(*i) + 1] == 34)
+	if (str[(*i) + 1] == 34)
 		(*i)++;
-	else if (argv[(*i) + 1] == ' ')
+	else if (str[(*i) + 1] == ' ')
 	{
 		(*i)++;
-		while (argv[(*i)] == ' ')
+		while (str[(*i)] == ' ')
 			(*i)++;
 	}
-	else if (argv[(*i)] == 39 && argv[(*i) + 1] && argv[(*i) + 1] != ' ')
+	else if (str[(*i)] == 39 && str[(*i) + 1] && str[(*i) + 1] != ' ')
 		(*i)++;
 }
 
 
-char	*ft_add_2rd_quote(t_mini *one_cmd, int *i, char *line, char *line_after)
+char	*ft_add_2rd_quote(t_mini *one_cmd, int *i, char *line, char *str)
 {
 	(void)one_cmd;
 	
@@ -98,21 +98,20 @@ char	*ft_add_2rd_quote(t_mini *one_cmd, int *i, char *line, char *line_after)
 	(*i)++;
 	while (line[*i] && line[*i] != '"')
 	{
-		line_after = ft_add_line_after(line_after, line[(*i)]);
+		str = ft_add_line_after(str, line[(*i)]);
 	//	printf("3.0 line_after = %s\n", line_after);
 		(*i)++;
 	}
-	printf("3. line_after in 3rd layer is: _%s_\n", line_after);
-	if (line_after)
-		ft_avs(one_cmd, line_after);
-	return (line_after);
+	printf("3. line_after in 3rd layer is: _%s_\n", str);
+	if (str)
+		ft_avs(one_cmd, str);
+	return (str);
 }
 
 
 
-char	*ft_d2_quotes(char *line_after, int *i, char *line, t_mini *one_cmd)
+char	*ft_d2_quotes(char *str, int *i, char *line, t_mini *one_cmd)
 {
-	char *str;
 	// useless
 	// if (line_after)
 	// {
@@ -121,7 +120,7 @@ char	*ft_d2_quotes(char *line_after, int *i, char *line, t_mini *one_cmd)
 	// 	line_after = NULL;
 	// }
 	// // done useless
-	str = ft_add_2rd_quote(one_cmd, i, line, line_after);
+	str = ft_add_2rd_quote(one_cmd, i, line, str);
 	if (str == NULL)
 		return (NULL);
 	return (str);
@@ -131,7 +130,7 @@ char	*ft_d2_quotes(char *line_after, int *i, char *line, t_mini *one_cmd)
 
 
 
-int	ft_add_2rd_s_quote(t_mini *one_cmd, int *i, char *line, char *line_after)
+int	ft_add_2rd_s_quote(t_mini *one_cmd, int *i, char *line, char *str)
 {
 	int		start;
 
@@ -139,28 +138,28 @@ int	ft_add_2rd_s_quote(t_mini *one_cmd, int *i, char *line, char *line_after)
 	(*i)++;
 	while (line[(*i)] && line[(*i)] != '\'')
 	{
-		line_after = ft_add_line_after(line_after, line[(*i)]);
+		str = ft_add_line_after(str, line[(*i)]);
 		(*i)++;
 	}
 	if (!ft_check_2rd_quote(&line[start], '\''))
 	{
-		free(line_after);
+		free(str);
 		printf("Error: Second single quote not found\n\n"); //g_n_exit = ??
 		return (0);
 	}
-	if (line_after)
-		ft_avs(one_cmd, line_after);
+	if (str)
+		ft_avs(one_cmd, str);
 	return (1);
 }
 
-int	ft_single_quote(char *line_after, int *i, char *line, t_mini *one_cmd)
+int	ft_single_quote(char *str, int *i, char *line, t_mini *one_cmd)
 {
-	if (line_after)
+	if (str)
 	{
-		ft_avs(one_cmd, line_after);
-		line_after = NULL;
+		ft_avs(one_cmd, str);
+		str = NULL;
 	}
-	if (!ft_add_2rd_s_quote(one_cmd, i, line, line_after))
+	if (!ft_add_2rd_s_quote(one_cmd, i, line, str))
 		return (0);
 	return (1);
 }
