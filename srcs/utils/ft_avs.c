f57fd51f1c1d81792325f0ae9e9401922f05d649
thi-phng <thi-phng@student.42.fr>
2022-02-17 17:49:53 +0100

    ok 2e version 1

--- srcs/utils/ft_avs.c
+++ srcs/utils/ft_avs.c
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 16:21:52 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/02/17 16:08:40 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/02/17 17:49:49 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,8 +110,6 @@ int	ft_avs(t_cmd *one_cmd, char *line_after)
 	int	len_tab;
 	int i = 0;
 
-
-	printf("Inside ft_avs\n");
 	len_tab = ft_len_avs(one_cmd->av);
 	printf("Start adding avs\n");
 	one_cmd->av = ft_malloc_avs(one_cmd, len_tab, line_after);
@@ -121,11 +119,7 @@ int	ft_avs(t_cmd *one_cmd, char *line_after)
 	printf("Done add av ft_avs\n");
 	
 	
-	while (i <= len_tab)
-	{
-		printf("\n			cmd->av[%d] = %s\n", i, one_cmd->av[i]);
-		i++;
-	}
+
 	return (1);
 }
 
