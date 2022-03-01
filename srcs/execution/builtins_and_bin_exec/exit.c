2ebd0ada4471908ab2a338f073bad1e7aefc2dd4
Marie-ines LOUBET <mloubet@e3r10p7.clusters.42paris.fr>
2022-03-01 19:07:41 +0100

    ok exit messages derreur

--- srcs/execution/builtins_and_bin_exec/exit.c
+++ srcs/execution/builtins_and_bin_exec/exit.c
@@ -2,15 +2,19 @@
 
 void	exec_exit(int ac, char **av)
 {
-	if (ac > 3)
+	if (ac > 2)
+	{
+		ft_puterror_fd("exit: ", "too many arguments", NULL);
+		return;
+	}
+	if (ac == 2)
 	{
 		if (!is_digit(av[1][0])) // a rechecker si chaque element de la string ou juste le premier
 		{
 			ft_putstr_fd("exit\n", 1);
 			ft_puterror_fd("minishell: exit: ", av[1], ":numeric argument required");
+			//exit quand meme
 		}
-		else
-			ft_puterror_fd("exit: ", "too many arguments", NULL);
 	}
 }
 
