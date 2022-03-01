ea267a19313e1778780f40fe70e02215695f82c0
thi-phng <thi-phng@student.42.fr>
2022-03-01 20:56:35 +0100

    ok quotes + redir error => dollar sign to do

--- minishell.h
+++ minishell.h
@@ -6,23 +6,12 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:34:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/03/01 20:28:31 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/03/01 20:56:26 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
-
-#define READ_END fd[0]
-#define WRITE_END fd[1]
-#define STDIN 0
-#define STDOUT 1
-#define EXIT_FAILURE 1
 
 # include <stdio.h>
 # include <unistd.h>
@@ -36,6 +25,14 @@
 # include <errno.h>  
 # include<sys/wait.h>
 # include <fcntl.h>
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
+
+# define READ_END fd[0]
+# define WRITE_END fd[1]
+# define STDIN 0
+# define STDOUT 1
+# define EXIT_FAILURE 1
 
 // redirection type
 # define NOPE_0 0
@@ -43,7 +40,6 @@
 # define HEREDOC_0 2
 # define TRUNC_0 3
 # define APPEND_0 4
-
 
 // others
 # define ERROR -1
@@ -53,7 +49,6 @@
 # define PIPE 2
 
 # define SEPARATORS " '\"|><"
-
 
 // *** // STRUCTURE  // *** //
 typedef struct s_export
@@ -133,14 +128,14 @@ void 		ft_putchar(int c);
 void 		ft_putstr(char *s);
 void  		ft_free_tab(char **tab);
 int			ft_len_avs(char **avs);
-int	ft_syntax_error(t_cmd *mini);
-int	skip_blank(char *str);
-char	**ft_env_cpy(char **envp);
-void print_mini_avs(t_mini *mini);
-int	rest_is_blank(char *str, int *i);
+int			ft_syntax_error(t_cmd *mini);
+int			skip_blank(char *str);
+char		**ft_env_cpy(char **envp);
+void 		print_mini_avs(t_mini *mini);
+int			rest_is_blank(char *str, int *i);
 
 
-char	*ft_strxjoin(char *s1, char *s2, char *s3);
+char		*ft_strxjoin(char *s1, char *s2, char *s3);
 //char		*ft_strdup(const char *s1);
 char		*ft_strjoin(char *s1, char *s2);
 int	    	ft_strlen(char *s);
@@ -150,7 +145,7 @@ int	    	chpos(const char *s, int c);
 void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
-int		ft_puterror_fd(char *error, char *s, char *error2);
+int			ft_puterror_fd(char *error, char *s, char *error2);
 void		free_tab(char ***line);
 int			ft_count_quotes(const char *str);
 char		*ft_strdup(char *s1);
@@ -164,35 +159,34 @@ void    	add_to_export_lst(t_export **export_lst, char *export_name, char *expor
 void		printstack(t_mini *env);
 void		ft_memdel(char **s);
 void		ft_free_lst(t_mini **head);
-void	run_builtin(t_mini *mini, t_cmd *cmd);
-void	run_execve_2(t_mini *mini, t_cmd *cmd);
+void		run_builtin(t_mini *mini, t_cmd *cmd);
+void		run_execve_2(t_mini *mini, t_cmd *cmd);
 
-
-int	exec_cd(int ac, char **av, char **env);
+int			exec_cd(int ac, char **av, char **env);
 
 //pwd
-int exec_pwd(void) ;
+int 		exec_pwd(void) ;
 /*echo*/
-int		is_option_n(char *av);
-int		echo(int ac, char **av, int option_n);
-int		exec_echo(int ac, char **av);
+int			is_option_n(char *av);
+int			echo(int ac, char **av, int option_n);
+int			exec_echo(int ac, char **av);
 /*env, export et unset*/
 char		*ft_strxjoin(char *s1, char *s2, char *s3);
 //char		**ft_split(char *s, char sep);
 //char		*find_cmd_path(char *cmd, char **env);
-int		env_realloc_and_append_envvar(char ***env, char *envvar);
+int			env_realloc_and_append_envvar(char ***env, char *envvar);
 char		*find_in_env(char **env, char *name, int *pos);
 char		*ft_getenv(char **env, char *name);
-int		ft_setenv(char ***env, char *av, char *name, char *value);
-int		get_into_export_lst(char ***env, char **av, char **name, char **data);
+int			ft_setenv(char ***env, char *av, char *name, char *value);
+int			get_into_export_lst(char ***env, char **av, char **name, char **data);
 void		init_tab(char **av, char ***s);
-int		exec_export(int ac, char **av, char ***env);
-int		ft_unsetenv(char ***env, char *name);
-int		exec_unset(int ac, char **av, char ***env);
+int			exec_export(int ac, char **av, char ***env);
+int			ft_unsetenv(char ***env, char *name);
+int			exec_unset(int ac, char **av, char ***env);
 void		print_env(char **env);
 char		*ft_getenv(char **env, char *name);
-void	print_env(char **env);
-void	 exec_exit(int ac, char **av);
+void		print_env(char **env);
+void		 exec_exit(int ac, char **av);
 void		ft_bzero(void *b, size_t n);
 void		*ft_memalloc(size_t size);
 void		test_print(char **envp);
@@ -212,68 +206,67 @@ void		print_tab(char **env);
 //char		**ft_copy_tab(char **env);
 int			ft_unsetenv(char ***env,char *name);
 void		print_export(char **tab);
-int     exec_cmd(int ac, char **av, char ***env);
-int exec_builtin(char *builtin, int ac, char **av, char ***env);
-int	is_builtin(char *builtin);
-int	is_builtin_2(t_mini *mini, t_cmd *cmd);
+int   	 	exec_cmd(int ac, char **av, char ***env);
+int			exec_builtin(char *builtin, int ac, char **av, char ***env);
+int			is_builtin(char *builtin);
+int			is_builtin_2(t_mini *mini, t_cmd *cmd);
 
 //*** PARSING ***//
 int			parsing(t_cmd *mini, char *line/*, t_cmd *cmd*/);
 int 		ft_pars_piping(char *line, t_cmd *mini);
-void			ft_each_cmd_2(t_mini *mini, int *i, t_cmd *cmd);
-int		ft_each_cmd(char *line, int *i, t_cmd *cmd);
+void		ft_each_cmd_2(t_mini *mini, int *i, t_cmd *cmd);
+int			ft_each_cmd(char *line, int *i, t_cmd *cmd);
 int			ft_avs(t_cmd *one_cmd, char *line_after);
-char	**ft_avs_2(t_cmd *one_cmd, char *line_after);
+char		**ft_avs_2(t_cmd *one_cmd, char *line_after);
 int			ft_buf(char *argv, int *i, char *buf);
 int			malloc_node(t_cmd	**one_cmd);
 int			ft_check_2rd_quote_2(char *line, int c);
-char	*ft_d2_quotes(char *line_after, int *i, char *line, t_cmd *cmd);
+char		*ft_d2_quotes(char *line_after, int *i, char *line, t_cmd *cmd);
 char		*ft_single_quote(char *line_after, int *i, char *line, t_cmd *one_cmd);
 void		free_avs(char **avs);
-char	*dollar_sign(int ac, char **av, char **env);
-void	skip_blank_2(char *str, int *i, t_cmd *tmp, char *line_after);
+char		*dollar_sign(int ac, char **av, char **env);
+void		skip_blank_2(char *str, int *i, t_cmd *tmp, char *line_after);
 
-
-void	ft_pass_squote(char *argv, int *i);
-int	quote_pass_2(char *argv, int *i);
+void		ft_pass_squote(char *argv, int *i);
+int			quote_pass_2(char *argv, int *i);
 
 char		*ft_add_line_after(char *line, char buf);
 
 t_cmd		*add_cell(t_cmd *mini, char *cmd, int pos);
 void		print_list(t_cmd **mini);
-int	is_blank(int c);
-int	is_redir(int c);
-void	get_line(t_mini *mini, int *pos, t_cmd *cmd);
-t_cmd	*stock_cmds(t_mini *mini);
-void	ft_each_cmd_3(t_mini *mini, char *str, int *i, t_cmd *cmd);
-t_cmd	*stock_cmds_2(t_mini *mini);
-int	ft_each_cmd_4(t_mini *mini, char *line, int *i, t_cmd **one_cmd);
-void	get_redir(t_mini *mini, int *i, t_cmd *cmd);
-char	*add_char(t_mini *mini, char *str, int c);
+int			is_blank(int c);
+int			is_redir(int c);
+void		get_line(t_mini *mini, int *pos, t_cmd *cmd);
+t_cmd		*stock_cmds(t_mini *mini);
+void		ft_each_cmd_3(t_mini *mini, char *str, int *i, t_cmd *cmd);
+t_cmd		*stock_cmds_2(t_mini *mini);
+int			ft_each_cmd_4(t_mini *mini, char *line, int *i, t_cmd **one_cmd);
+void		get_redir(t_mini *mini, int *i, t_cmd *cmd);
+char		*add_char(t_mini *mini, char *str, int c);
 
 
 //*** PIPES ***//
-char	*ft_strndup(char *s, int n);
-char	**ft_split(char *s, char sep);
-void print_tab(char **av);
-void print_cmds(t_cmd *cmd);
-int  nb_cmds(t_cmd *cmd);
+char		*ft_strndup(char *s, int n);
+char		**ft_split(char *s, char sep);
+void 		print_tab(char **av);
+void 		print_cmds(t_cmd *cmd);
+int  		nb_cmds(t_cmd *cmd);
 
-void safely_exec_bin(char **cmd);
-void safely_pipe_me(int new_pipe_fd[]);
-void safely_fork(int *pid);
+void 		safely_exec_bin(char **cmd);
+void 		safely_pipe_me(int new_pipe_fd[]);
+void 		safely_fork(int *pid);
 
-void	child_process(t_cmd *cmd, int *fd, t_mini *mini);
-void waiting_for_all_children_to_finish_execution(pid_t	pid_lst[]);
-void run_piped_cmds(t_mini *mini, int nb_cmd);
+void		child_process(t_cmd *cmd, int *fd, t_mini *mini);
+void 		waiting_for_all_children_to_finish_execution(pid_t	pid_lst[]);
+void 		run_piped_cmds(t_mini *mini, int nb_cmd);
 
-void exec_cmd_with_no_pipe(t_mini *mini);
+void 		exec_cmd_with_no_pipe(t_mini *mini);
 
 //*** REDIRECTIONS ***//
-void    ft_set_direct(char *line, int *i, t_cmd *mini);
-void    get_redir_in(t_mini *mini, int i, t_cmd *cmd, char *line);
-void    get_redir_out(t_mini *mini, int i, t_cmd *cmd);
-void  ft_print_av(t_cmd *mini);
+void    	ft_set_direct(char *line, int *i, t_cmd *mini);
+void    	get_redir_in(t_mini *mini, int i, t_cmd *cmd, char *line);
+void    	get_redir_out(t_mini *mini, int i, t_cmd *cmd);
+void  		ft_print_av(t_cmd *mini);
 
 //*** SIGNAUX ***//
 
@@ -292,7 +285,7 @@ void    ft_start_signal(void);
 
 
 // simple utils
-int	is_alnum(int c);
+int		is_alnum(int c);
 //
 
 void	ft_free_cmds(t_mini *mini);
