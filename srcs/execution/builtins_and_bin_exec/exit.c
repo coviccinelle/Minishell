3c70470c6a6ab0d611ccd30ced1305818d27e928
thi-phng <thi-phng@student.42.fr>
2022-03-02 14:32:13 +0100

    add exit + last file in and out + redirection on mutiple pipes

--- srcs/execution/builtins_and_bin_exec/exit.c
+++ srcs/execution/builtins_and_bin_exec/exit.c
@@ -1,18 +1,19 @@
 #include "../../../minishell.h"
 
-void	exec_exit(int ac, char **av)
+void    exec_exit(int ac, char **av)
 {
-(void)ac;
-(void)av;
-/*	if (ac > 3)
-	{
-		if (!is_digit(av[1])) // a rechecker si chaque element de la string ou juste le premier
-			ft_puterror_fd("exit: ", av[1], ":numeric argument required");
-		else
-			ft_puterror_fd("exit: ", "too many arguments", NULL);
-	}
-	//EXIT
-
-*/
+    if (ac > 2)
+    {
+        ft_puterror_fd("exit: ", "too many arguments", NULL);
+        return;
+    }
+    if (ac == 2)
+    {
+        if (!is_digit(av[1][0])) // a rechecker si chaque element de la string ou juste le premier
+        {
+            ft_putstr_fd("exit\n", 1);
+            ft_puterror_fd("minishell: exit: ", av[1], ":numeric argument required");
+            //exit quand meme
+        }
+    }
 }
-
