07b4066e7838232644d0f0feff13cad8fd858659
Marie-ines LOUBET <mloubet@e2r7p19.clusters.42paris.fr>
2022-03-04 18:52:04 +0100

    debut norminette utils

--- srcs/utils/utils_libft_1.c
+++ srcs/utils/utils_libft_1.c
@@ -1,13 +1,13 @@
 #include "../../minishell.h"
 
-int is_digit(char c)
+int	is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
 
-int is_alpha(char c)
+int	is_alpha(char c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
@@ -26,12 +26,12 @@ int	ft_strlen(char	*s)
 	return (i);
 }
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-        int     i;
-        int     j;
-        int     size;
-        char    *res;
+        int	i;
+        int	j;
+        int	size;
+        char	*res;
 
         i = 0;
         j = 0;
@@ -39,22 +39,17 @@ char    *ft_strjoin(char *s1, char *s2)
         res = (char *)malloc(sizeof(char) * size);
         if (!res || (!s1 && !s2))
                 return (NULL);
-//      if(s1)
-  //              printf("\n S1 : %s\n",s1);
-    //    if(s2)
-      //         printf("\n S2 : %s\n", s2);
-  
 	if (!s2)
 		return (res = ft_strndup(s1, ft_strlen(s1)));
-        else if(!s1)
-                return (res = ft_strndup(s2, ft_strlen(s2)));
-        while (s1[i])
-                res[j++] = s1[i++];
-        i = 0;
-        while (s2[i])
-                res[j++] = s2[i++];
-        res[j] = '\0';
-        return (res);
+        else if (!s1)
+		return (res = ft_strndup(s2, ft_strlen(s2)));
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	return (res);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -74,5 +69,3 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (&str[i]);
 }
-
-
