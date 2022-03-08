bfdfcc3c07f2f636b49ae21f75464c8d7d4fb5fc
thi-phng <thi-phng@student.42.fr>
2022-03-08 12:00:21 +0100

    fixed unclosed quotes segfaut again + echo espace entre quote

--- srcs/parsing/stock_cmd.c
+++ srcs/parsing/stock_cmd.c
@@ -1,18 +1,5 @@
 #include "../../minishell.h"
 
-// should be main parsing -> pour la norme?
-void	get_line(t_mini *mini, int *i, t_cmd *cmd)
-{
-	//char	*var_val;
-	//int		quote_type;
-
-	//var_val = NULL;
-	while (mini->line[*i] && !is_redir(mini->line[*i]) && mini->line[*i] != '|')
-	{
-		cmd->line = ft_add_line_after(cmd->line, mini->line[(*i)++]);
-	}
-}
-
 t_cmd	*new_elem_cmd(t_mini *mini)
 {
 	t_cmd	*elem;
@@ -49,7 +36,6 @@ void	add_cmd(t_cmd **cmd_lst, t_cmd *cmd)
 	cmd->prev = current;
 }
 
-
 char	*get_var_name(char *s, int *i)
 {
 	char	*var_name;
@@ -84,79 +70,6 @@ void	get_var_dollar(t_mini *mini, t_cmd *cmd, char *s, int *i)
 }
 
 
-// int	ft_quote(t_mini *mini, t_cmd *cmd, char *s, int *i)
-// {
-// 	char	*line_after;
-
-// 	(void)mini; // to use after for $ sign inside quote
-// 	line_after = NULL;
-// 	if (s[*i] == '"')
-// 	{
-// 		printf("1_Double quote found\n\n");
-// 		printf("where am i ? s[*i] = double quote found : %c\n", s[*i]);
-// 		if (!ft_d2_quotes(line_after, i, s, cmd))
-// 			return (0);
-// 		printf("cmd->av[0] = %s\ncmd->av[1] = %s\n", cmd->av[0], cmd->av[1]);
-// 		// if (s[(*i) + 1] == '\0')
-// 		// 	return (0) ;
-// 		//dollar in quote
-// 		if (!quote_pass_2(s, i))
-// 			return (0) ;
-// 		line_after = NULL;
-// 	}
-// 	else if (s[*i] == '\'')
-// 	{
-// 		printf("single quotes\n\n");
-// 		printf("line_after = %s\n", line_after);
-// 		if (!ft_single_quote(line_after, i, s, cmd))
-// 			return (0);
-// 		// if (s[(*i) + 1] == '\0')
-// 		// 	break ;
-// 		ft_pass_squote(s, i);
-// 		line_after = NULL;
-// 	}
-// 	return (1);
-// }
-
-
-// void	get_avs(t_mini *mini, int *i, t_cmd *cmd)
-// {
-// 	char	*line;
-// 	char	*s;
-// 	char	*buf;
-
-// 	line = NULL;
-// 	s = mini->line;
-// 	printf("get_avs : 1.0");
-// 	while (s[*i] && !is_redir(s[*i]) && s[*i] != '|' && s[*i] != ' ')
-// 	{
-// 		if (s[*i] == '$')
-// 			get_var_dollar(mini, cmd, s, i);//a fuction to check if variable after $ exist in env + stock avs
-// 		else if (s[*i] == '"' || s[*i] == '\'')
-// 		{
-// 			if (!ft_quote(mini, cmd, s, i)) // call funtion ft_quotes to take all entre les quotes + stock avs
-// 			{
-// 				printf("ERROR: quote not ok\n");
-// 				exit (0);
-// 				//exit_custom;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			printf("mini->line[i] = %c\n", mini->line[*i]);
-// 			buf = malloc(sizeof(char) * 2);
-// 			ft_buf(s, i, buf);
-// 			line = ft_add_line_after(line, buf[0]);
-// 			printf("cmd->line = %s\n", line);
-// 			if (!s[*i] && line)
-// 				ft_avs(cmd, line);
-// 			free(buf);
-// 		}
-// 	}
-// }
-
-
-
 int			create_files(int type, char *filename)
 {
     int fd;
@@ -183,10 +96,6 @@ t_file    *ft_last_file(t_file *file)
     t_file       *p;
 
     p = file;
-
-   // printf("am i here 2222?\n");
-    /*if (p && p->next == NULL)
-            return (p);*/
     while (p && p->next)
     {
         printf("filename  = %s\n", p->name);
@@ -220,20 +129,6 @@ void    stock_cmds_3(t_cmd *cmd)
         printf("le dernier fichier OUT est : type %d nom = %s\n\n", cmd->last_file_out->type, cmd->last_file_out->name);
 }
 
-// void	stock_cmds_3(t_cmd *cmd)
-// {
-// 	while ((cmd)->file_in)
-// 	{
-// 		create_files((*cmd).file_in->type, (cmd)->file_in->name);
-// 		(cmd)->file_in = (cmd)->file_in->next;
-// 	}
-// 	while ((cmd)->file_out)
-// 	{
-// 		create_files((*cmd).file_out->type, (cmd)->file_out->name);
-// 		(cmd)->file_out = (cmd)->file_out->next;
-// 	}
-// }
-
 //stock cmd
 t_cmd	*stock_cmds(t_mini *mini)
 {
@@ -261,8 +156,6 @@ t_cmd	*stock_cmds(t_mini *mini)
 			{
 				printf("ERROR: syntax error => free tout\nValuer de retour???\n\n");
 				return (NULL);
-			//	mini->stop = 1;
-			//	break ;
 			}
 		}
 		if (mini->line[i] == '|')
